3dc3838aa15f21320361ccc425aadf3ec788907f
w0nght <wwongheitung@gmail.com>
2019-01-24 15:44:27 +1100

    android settings and all build files

--- android/app/src/main/java/com/pleasework/MainApplication.java
+++ android/app/src/main/java/com/pleasework/MainApplication.java
@@ -8,11 +8,11 @@ import com.facebook.appevents.AppEventsLogger;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
 
 import java.util.Arrays;
 import java.util.List;
@@ -34,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
           new FBSDKPackage(mCallbackManager)
       );
     }
