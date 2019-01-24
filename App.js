664d8a84d8357df25d3779c0246e37f6ee202866
w0nght <wwongheitung@gmail.com>
2019-01-24 15:39:43 +1100

    changes in app js

--- App.js
+++ App.js
@@ -6,33 +6,99 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, ScrollView } from 'react-native';
+import { 
+createStackNavigator, 
+// createSwitchNavigator, 
+createDrawerNavigator, 
+createAppContainer } from 'react-navigation' //@3.0.9
 
-var FBLoginButton = require('./app/components/FBLoginButton');
+import LoginScreen from './app/views/LoginScreen'
+import Register from './app/views/RegisterScreen'
+import Regi2 from './app/views/RegiForm'
+import re3 from './app/views/re3'
+import quicktest from './app/views/quicktest'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// var FBLoginButton = require('./app/components/FBLoginButton');
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <FBLoginButton />
+    return <AppContainer/>;
+  }
+}
 
+class Home extends React.Component {
+  static navigationOptions = {
+    headerStyle: {
+      backgroundColor: 'green'
+    }
+  };
+  render() {
+    return( 
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Home!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
       </View>
     );
   }
 }
+class Dashboard extends React.Component {
+  static navigationOptions = {
+    headerStyle: {
+      backgroundColor: 'white'
+    }
+  };
+  render() {
+    return( 
+      <ScrollView>
+        <View style={{ height: 1000, borderWidth: 15, borderColor: 'green'}} />
+      </ScrollView>
+    );
+  }
+}
 
+const AppStackNavigator = createStackNavigator({
+  Home: Home
+},{
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: 'orange'
+    }
+  }
+});
+
+const AppDrawerNavigator = createDrawerNavigator({
+  Home: Home,
+  Dashboard: Dashboard,
+  LoginScreen: LoginScreen,
+  Register: Register,
+  Regi2: Regi2,
+  re3: re3,
+  quicktest: quicktest
+},{
+  unmountInactiveRoute: true,
+  initialRouteName: 'quicktest',
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: 'orange'
+    }
+  }
+});
+
+const AppContainer = createAppContainer(AppDrawerNavigator);
+
+/*const AuthStackNavigator = createStackNavigator({
+  Splash: SplashScreen,
+  Login: LoginScreen,
+})
+
+export default createSwitchNavigator({
+  AuthLoading: AuthLoadingScreen,
+  Auth: AuthStackNavigator,
+
+})
+*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
